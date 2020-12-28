16ccdc291e19785a6c01bf278fe15ed9470d730d
bhara <bharathJ222000@gmail.com>
2020-12-28 21:53:21 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+const typeDefs = require('./graphql/typedefs');
+const resolvers = require('./graphql/resolvers/index');
+const { DB_URL } = require('./config.js');
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req })
+});
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database connected'.yellow.bold);
+        return server.listen({ port: PORT });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`.cyan.bold);
+    })
+    .catch((err) => {
+        console.log('Error Occured'.red.bold + err);
+    })
